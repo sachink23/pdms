f57c915b567f68e5aa93d0dfcb7e201a43446f5d
Sachin Kekarjawalekar <sachin@kekarjawalekar.com>
2020-11-12 11:26:44 +0530

    Getting Updates
    Signed-off-by: Sachin Kekarjawalekar <sachin@kekarjawalekar.com>

---
+++ district/reports.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__."/../include.php";
+
+$title = APP_SHORT_NAME . " : Reports";
+
+
+
+require_once __DIR__. "/chunks/top.php";
+
+$db = new Db();
+$con = $db->con();
+
+?>
+    <h1>Under Development!</h1>
+<?php
+
+require_once __DIR__."/chunks/bottom.php";
+
+?>
