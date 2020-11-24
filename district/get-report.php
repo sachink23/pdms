a6b43082255a359ee5c0bbc4ad5f584f1541d51c
Sachin Kekarjawalekar <sachin@kekarjawalekar.com>
2020-11-24 17:24:29 +0530

    Updated Reports
    Signed-off-by: Sachin Kekarjawalekar <sachin@kekarjawalekar.com>

---
+++ district/get-report.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once __DIR__."/../include.php";
+
+$title = APP_SHORT_NAME . " : District Dashboard";
+
+
+
+require_once __DIR__. "/chunks/top.php";
+$db = new Db();
+$con = $db->con();
+
+$show = true;
+$slot = "";
+if ($_GET["slot"] == "slot_1") {
+    $stmt = "SELECT b.booth_number, b.booth_name, s.male_voters as male_voting_slot, s.fmale_voters as female_voting_slot, s.t_voters as trans_voting_slot, b.num_voters as total_voters, b.male_voters as total_male_voters, b.female_voters as total_female_voters, b.t_voters as total_trans_voters FROM booths b left outer join slot_1 s on b.booth_id = s.booth_id";
+    $slot = "UPTO 10 AM";
+} else if ($_GET["slot"] == "slot_2") {
+    $stmt = "SELECT b.booth_number, b.booth_name, s.male_voters as male_voting_slot, s.fmale_voters as female_voting_slot, s.t_voters as trans_voting_slot, b.num_voters as total_voters, b.male_voters as total_male_voters, b.female_voters as total_female_voters, b.t_voters as total_trans_voters FROM booths b left outer join slot_2 s on b.booth_id = s.booth_id";
+    $slot = "UPTO 12 PM";
+} else if ($_GET["slot"] == "slot_3") {
+    $stmt = "SELECT b.booth_number, b.booth_name, s.male_voters as male_voting_slot, s.fmale_voters as female_voting_slot, s.t_voters as trans_voting_slot, b.num_voters as total_voters, b.male_voters as total_male_voters, b.female_voters as total_female_voters, b.t_voters as total_trans_voters FROM booths b left outer join slot_3 s on b.booth_id = s.booth_id";
+    $slot = "UPTO 2 PM";
+} else if ($_GET["slot"] == "slot_4") {
+    $stmt = "SELECT b.booth_number, b.booth_name, s.male_voters as male_voting_slot, s.fmale_voters as female_voting_slot, s.t_voters as trans_voting_slot, b.num_voters as total_voters, b.male_voters as total_male_voters, b.female_voters as total_female_voters, b.t_voters as total_trans_voters FROM booths b left outer join slot_4 s on b.booth_id = s.booth_id";
+    $slot = "UPTO 4 PM";
+} else if ($_GET["slot"] == "slot_5") {
+    $stmt = "SELECT b.booth_number, b.booth_name, s.male_voters as male_voting_slot, s.fmale_voters as female_voting_slot, s.t_voters as trans_voting_slot, b.num_voters as total_voters, b.male_voters as total_male_voters, b.female_voters as total_female_voters, b.t_voters as total_trans_voters FROM booths b left outer join slot_5 s on b.booth_id = s.booth_id";
+    $slot = "FINAL";
+}
+else {
+    pageInfo("warning", "Invalid Selection!");
+    $show = false;
+}
+if ($show):
+    $query = $con->query($stmt);
+    $res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<div class="container-fluid table-responsive">
+    <h3 class="text-center mb-3">Report : <?= $slot ?></h3>
+    <table class="table table-bordered text-center">
+        <thead class="thead-default">
+        <tr>
+            <th rowspan="2">Sr Number</th>
+            <th rowspan="2">Booth Number</th>
+            <th rowspan="2">Booth Name</th>
+            <th rowspan="2">Total Votes</th>
+            <th colspan="4">Casted Votes</th>
+            <th rowspan="2">% Voting</th>
+        </tr>
+        <tr>
+            <th>Male</th>
+            <th>Female</th>
+            <th>Transgender</th>
+            <th>Total</th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php
+            $total_votes = 0;
+            $male_votes_casted = 0;
+            $female_votes_casted = 0;
+            $trans_votes_casted = 0;
+
+            $i = 0; foreach ($res as $booth):
+                $male_voting = $booth["male_voting_slot"] ?? 0;
+                $female_voting = $booth["female_voting_slot"] ?? 0;
+                $trans_voting = $booth["trans_voting_slot"] ?? 0;
+                $total_votes += $booth["total_voters"];
+                $votes_casted = $male_voting + $female_voting + $trans_voting;
+                $male_votes_casted += $male_voting;
+                $female_votes_casted += $female_voting;
+                $trans_votes_casted += $trans_voting;
+
+                ?>
+            <tr>
+                <td><?= ++$i ?></td>
+                <td><?= $booth["booth_number"] ?></td>
+                <td><?= $booth["booth_name"] ?></td>
+                <td><?= $booth["total_voters"] ?></td>
+                <td><?= $male_voting ?></td>
+                <td><?= $female_voting ?></td>
+                <td><?= $trans_voting ?></td>
+                <td><?= $votes_casted ?></td>
+                <td><?= round(100 * ($votes_casted/$booth["total_voters"]), 4)  ?></td>
+
+
+            </tr>
+            <?php endforeach;
+                $total_votes_casted = $male_votes_casted + $female_votes_casted + $trans_votes_casted;
+            ?>
+            <tr>
+                <th colspan="3">Total</th>
+                <th><?= $total_votes ?></th>
+                <th><?= $male_votes_casted ?></th>
+                <th><?= $female_votes_casted ?></th>
+                <th><?= $trans_votes_casted ?></th>
+                <th><?= $total_votes_casted ?></th>
+                <th><?= round(100 * ($total_votes_casted/$total_votes), 4) ?></th>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php
+    endif;
+    require_once "chunks/bottom.php";
+?>
--- district/reports.php
+++ district/reports.php
@@ -12,7 +12,15 @@ $db = new Db();
 $con = $db->con();
 
 ?>
-    <h1>Under Development!</h1>
+    <h1>Reports</h1><hr />
+    <div class="list-group">
+        <a href="get-report.php?slot=slot_1" class="list-group-item list-group-item-action">Report UPTO 10 AM</a>
+        <a href="get-report.php?slot=slot_2" class="list-group-item list-group-item-action">Report UPTO 12 PM</a>
+        <a href="get-report.php?slot=slot_3" class="list-group-item list-group-item-action">Report UPTO 2 PM</a>
+        <a href="get-report.php?slot=slot_4" class="list-group-item list-group-item-action">Report UPTO 4 PM</a>
+        <a href="get-report.php?slot=slot_5" class="list-group-item list-group-item-action">Report Final</a>
+    </div>
+
 <?php
 
 require_once __DIR__."/chunks/bottom.php";
